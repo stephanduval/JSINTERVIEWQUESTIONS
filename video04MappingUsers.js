9b08ad3212e6187ee939f5abd8bbb7bf2ff4fc2f
Stephan DuVal <stephan.duval@gmail.com>
2023-02-13 15:22:43 -0800

    Finished Video 04

---
+++ video04MappingUsers.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    id: 1,
+    name: "Jack",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "John",
+    isActive: true,
+  },
+  {
+    id: 3,
+    name: "Mike",
+    isActive: false,
+  },
+];
+
+function createUserNameList(userList) {
+  const userNameList = [];
+  userList.forEach((element) => userNameList.push(element.name));
+  return userNameList;
+}
+createUserNameList(users);
+console.log(createUserNameList(users));
+
+function createUserNameListWithMap(userList) {
+  const namesList = users.map((element) => element.name);
+  console.log(namesList);
+}
+
+createUserNameListWithMap(users);
